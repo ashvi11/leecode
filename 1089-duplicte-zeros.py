c5a7c1f7fcff5230dd55c36dce16c79487aae1a0
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2023-02-27 21:48:28 -0500

    Create 1089-duplicte-zeros.py

---
+++ 1089-duplicte-zeros.py
@@ -0,0 +1,13 @@
+# whenever we encounter a '0', we 'pop' an element(delete from the end of array), and 'insert' 0 at the current location. Skip 2 indexes, and go to next 
+# index. Do this until i < len(arr). If we encounter 1, go to next index.
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                arr.pop()
+                arr.insert(i, 0)
+                i += 2
+            else:
+                i += 1
